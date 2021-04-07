1be62317f3384db6ff84e2959f0d58f45b7a6e91
Lucas Santos <lucas.parkl@outlook.com>
2021-04-07 19:02:42 -0300

    Delete GenericTree directory

--- GenericTree/src/exception/BoundaryViolationException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-@SuppressWarnings("serial")
-public class BoundaryViolationException extends RuntimeException {
-
-	public BoundaryViolationException(String err) {
-		super(err);
-	}
-}
--- GenericTree/src/interfaces/Position.java
+++
@@ -1,7 +0,0 @@
-package interfaces;
-
-public interface Position<E> {
-	// Retorna o elemento armazenado nesta posi��o.
-	E element();
-	
-}
